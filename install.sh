e61cb7592573438ab7149d3da2cbb365a23764b4
lnsf <lnsf03@gmail.com>
2019-06-27 22:07:46 +0900

    :bug: Fix install script

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 # DO NOT RUN THIS FILE DIRECTLY
 # USE make install
-FLAG=ctf4b-kosen{y0u_g0t_th1s_flag}
+FLAG="ctf4b-kosen{y0u_g0t_th1s_flag}"
 
-./exec-encrypt $FLAG ./code > .hint
-if [ ! -d "output" ];
-then
-    mkdir output
-fi
+./exec-encrypt $FLAG ./code > ./.hint
+
+mkdir output
 mv .hint exec-me output/
